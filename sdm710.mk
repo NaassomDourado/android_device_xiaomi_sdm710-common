ac88c6f5acabdf4fff0f7ae69b7b1714fdc8bf04
DarkAngelGR <asavvo01@gmail.com>
2021-06-27 18:41:07 +0000

    sdm710-common: Prepare for libperfmgr
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -280,15 +280,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.2.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
